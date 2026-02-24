00616c4e13e503bc003215e635b88683d061e4bf
Karma <dhorizenkarma@gmail.com>
2026-02-23 20:11:37 -0500

    Update codegabe.sh

--- codegabe.sh
+++ codegabe.sh
@@ -1,13 +1,18 @@
 #!/bin/bash/
-# ===================Command Starts HERE===================================================== 
+# ===================Command Starts HERE=====================================================
 sudo pacman -Syu
 #====================Drivers================================================================
 sudo pacman -S mesa -y
 sudo pacman -S libva-mesa-driver -y
 sudo pacman -S vulkan-radeon -y
 sudo pacman -S flatpak -y
+sudo pacman -S fuse -y
 #===================Games And Apps========================================
+cd
 cd Desktop
-curl -Lo https://curseforge.overwolf.com/downloads/curseforge-latest-linux.AppImage
-flatpak remote-add --user --if-not-exists geforcenow https://international.download.nvidia.com/GFNLinux/flatpak/geforcenow.flatpakrepo
+curl -O https://curseforge.overwolf.com/downloads/curseforge-latest-linux.AppImage
+sudo chmod +755  curseforge-latest-linux.AppImage
+curseforge-latest-linux.appImage
+flatpak remote-add --user --if-not-exists geforcenow https://international.download.nvidia.com/GFNLinux/flatp>
+flatpak install -y org.freedesktop.Platform/x86_64/24.08
 flatpak install --user -y geforcenow com.nvidia.geforcenow
